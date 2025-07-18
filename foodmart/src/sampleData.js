90ee0ce67f1be0abdaf231f691d844070628256f
nguyenanlc9 <139008945+nguyenanlc9@users.noreply.github.com>
2025-07-18 12:08:51 +0700

    docs: update dev command in README

---
+++ foodmart/src/sampleData.js
@@ -0,0 +1,7 @@
+const sampleData = [
+  { id: '1', name: 'Rau cải xanh', weight: '500g', price: 20000, image: 'https://via.placeholder.com/150' },
+  { id: '2', name: 'Táo đỏ', weight: '1kg', price: 50000, image: 'https://via.placeholder.com/150' },
+  { id: '3', name: 'Thịt bò', weight: '1kg', price: 150000, image: 'https://via.placeholder.com/150' },
+]
+
+export default sampleData
